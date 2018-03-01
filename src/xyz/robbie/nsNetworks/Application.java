5bf1d85ee313111c395dc296da7434665e30477c
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-03-01 11:50:53 +0000

    Fiddling with packages

--- src/xyz/robbie/nsNetworks/Application.java
+++ src/xyz/robbie/nsNetworks/Application.java
@@ -1,5 +1,10 @@
+package xyz.robbie.nsNetworks;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
